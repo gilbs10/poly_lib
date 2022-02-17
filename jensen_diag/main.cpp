fb6b49310e44256c2208f28d4667eecf28d1ffe7
gilbs10 <gilbs11@gmail.com>
2022-02-17 18:36:23 +0200

    User input

--- jensen_diag/main.cpp
+++ jensen_diag/main.cpp
@@ -20,12 +20,12 @@ int res_mul(int i, int j){
     return 1;
 }
 
-void count(int n){
+void count(int n, int num_of_threads){
     u128_addable c = 0;
     for (int wm = 0; wm < 2; ++wm) {
         for (int k = 2; k < n+1; ++k) {
 //           RectManager* rm = new RectManager(k, n, bool(wm));
-             RectManagerParallel* rm = new RectManagerParallel(k, n, bool(wm), 3);
+             RectManagerParallel* rm = new RectManagerParallel(k, n, bool(wm), num_of_threads);
             rm->run_rectangle();
             for (int j = k; j <= n; ++j) {
                 if (rm->res->find(j) != rm->res->end()){
@@ -62,12 +62,12 @@ void save_res(int n){
 
 
 
-void run_all(int n = 10) {
+void run_all(int n = 10, int num_of_threads = 1) {
     for (int i = 3; i <= n; ++i){
         auto timenow =
                 chrono::system_clock::to_time_t(chrono::system_clock::now());
         cout << "Running " << i << ", time: " << ctime(&timenow) << endl;
-        count(i);
+        count(i, num_of_threads);
     }
     auto timenow =
             chrono::system_clock::to_time_t(chrono::system_clock::now());
@@ -75,10 +75,12 @@ void run_all(int n = 10) {
 }
 
 int main(){
+    int n, num_of_threads;
     cout << "Enter size to run until:" << endl;
-    int n;
     cin >> n;
-    run_all(n);
+    cout << "Enter num of threads to run on:" << endl;
+    cin >> num_of_threads;
+    run_all(n, num_of_threads);
 //    int k = 20;
 //    int n = 20;
 //    bool wm = false;
