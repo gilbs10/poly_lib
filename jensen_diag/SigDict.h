531787ccb2f2466394d22772a42847f54dfd2ad7
gilbs10 <gilbs11@gmail.com>
2022-02-24 16:45:52 +0200

    CMAKE file

--- jensen_diag/SigDict.h
+++ jensen_diag/SigDict.h
@@ -8,8 +8,10 @@
 #include "BoundryPattern.h"
 #include "genfunc.h"
 #include "robin_hood.h"
+#include "abseil-cpp/absl/container/flat_hash_map.h"
 
-typedef robin_hood::unordered_map<sig, GenFunc> sig_map;
+//typedef robin_hood::unordered_map<sig, GenFunc> sig_map;
+typedef absl::flat_hash_map<sig, GenFunc> sig_map;
 
 class SigDict{
 public:
