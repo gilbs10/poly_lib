a43b7286a345cec56f0050ee8f143bb6b1d7f3db
gilbs10 <gilbs11@gmail.com>
2022-03-03 13:50:01 +0200

    Symmetry imporvements

--- jensen_diag/settings.h
+++ jensen_diag/settings.h
@@ -12,8 +12,8 @@ using namespace std;
 //The GenFunc class is the bottleneck of the program, thus we modify the code using defines to make no redundant code
 //Only use packing if not using preallocated array
 //#define GF_USE_PREALLOCATED
-#define GF_CONST_SIZE 11
-//#define GF_USE_PACKING
+//#define GF_CONST_SIZE 11
+#define GF_USE_PACKING
 //#define GF_MOD_CONST 4611686018427387903
 //#define GF_MOD_CONST 36028797018963968
 #define GF_MOD_CONST 2147483648
